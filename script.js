8ed11adc4a950eefb02d43fd9f25e80262c2fe19
Wojciech Tylenda <wojciech.tylenda.96@gmail.com>
2024-12-05 20:11:33 +0100

    finaly version of extension
    added button "save tab" to save actual open tab's url to leads

--- script.js
+++ script.js
@@ -4,30 +4,41 @@ const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+let url = window.location.href
 // functions
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value =""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads();
+    render(myLeads);
     console.log(localStorage.getItem("myLeads"))
 })
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear();
     myLeads = [];
-    renderLeads();
+    render(myLeads);
 })
 
-function renderLeads() {
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        const currentTabUrl = tabs[0].url;  // Pobieranie URL aktywnej karty
+        myLeads.push(currentTabUrl);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);  // Aktualizacja wyświetlanej listy
+    });
+})
+
+function render(leads) {
     let listItems = ""
-    for (i = 0; i < myLeads.length; i++) {
+    for (i = 0; i < leads.length; i++) {
         listItems += `
             <li id="li-el">
-                <a id="a-el" href="${myLeads[i]}" target="_blank">
-                    ${myLeads[i]}
+                <a id="a-el" href="${leads[i]}" target="_blank">
+                    ${leads[i]}
                 </a>
             </li>`
     }
@@ -36,6 +47,5 @@ function renderLeads() {
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
 }
-
