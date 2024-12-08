4e8bc0d4cfa9457111d55f388850dd329e30694c
Wojciech Tylenda <wojciech.tylenda.96@gmail.com>
2024-12-08 19:05:43 +0100

    add database to project instead localstorage

--- script.js
+++ script.js
@@ -1,50 +1,58 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getDatabase,
+         ref, 
+         push,
+         onValue,
+         remove } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURL: "https://lead-tracker-app-bda66-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+console.log
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const referenceInDB = ref(database, "leads")
+
+console.log(database)
+
 // variables and constants
-let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
-const tabBtn = document.getElementById("tab-btn");
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 // functions
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    inputEl.value =""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads);
-    console.log(localStorage.getItem("myLeads"))
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+onValue(referenceInDB, function(snapshot) {
+    let snapshotDoesExist = snapshot.exists();
+    if(snapshotDoesExist) {
+        const snapshotValues = snapshot.val();
+        const leads = Object.values(snapshotValues);
+        render(leads);
+    }
 })
 
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear();
-    myLeads = [];
-    render(myLeads);
+    remove(referenceInDB);
+    ulEl.innerHTML = "";
 })
 
-tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        const currentTabUrl = tabs[0].url;  // Pobieranie URL aktywnej karty
-        myLeads.push(currentTabUrl);
-        localStorage.setItem("myLeads", JSON.stringify(myLeads));
-        render(myLeads);  // Aktualizacja wyświetlanej listy
-    });
+inputBtn.addEventListener("click", function() {
+    push(referenceInDB ,inputEl.value)
+    inputEl.value = ""
 })
-
-function render(leads) {
-    let listItems = ""
-    for (i = 0; i < leads.length; i++) {
-        listItems += `
-            <li id="li-el">
-                <a id="a-el" href="${leads[i]}" target="_blank">
-                    ${leads[i]}
-                </a>
-            </li>`
-    }
-    ulEl.innerHTML = listItems;
-}
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage;
-    render(myLeads);
-}
